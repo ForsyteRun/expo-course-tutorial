7f1c5c00dd454a3262001962bee40fc81f34f539
Forsyte <forsyterun@gmail.com>
2025-08-25 16:09:32 +0200

    feat: fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useFonts({
+    'Roboto-Black': require('./../assets/fonts/RobotoCondensed-Black.ttf'),
+    'Roboto-Regular': require('./../assets/fonts/RobotoCondensed-Regular.ttf'),
+    'Roboto-Italic': require('./../assets/fonts/RobotoCondensed-Italic.ttf')
+  });
+
   return <Stack screenOptions={{ headerShown: false }}></Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import "./../global.css";
 
 export default function Index() {
+
   return (
     <SafeAreaView
       className="flex-1 bg-WHITE"
@@ -12,11 +13,11 @@ export default function Index() {
       <View className="flex-1 items-center justify-center ">
         <Image source={require("./../assets/images/landing.jpg")} className="w-full h-full" />
       </View>
-      <View className="flex items-center justify-start h-[55%] bg-PRIMARY rounded-t-xl p-10">
-        <Text className="text-center text-2xl text-WHITE pb-2" >Welcome to Course-Tutorial</Text>
-        <Text className="text-center text-sm text-WHITE pb-4">Transform your ideas into engaging learning experiences with AI 🍏 📖</Text>
-        <Button className="w-full capitalize  bg-WHITE py-2 mb-5" textClassName="text-sm">Get Started</Button>
-        <Button className="w-full capitalize bg-PRIMARY border border-WHITE py-2 mb-7" textClassName="text-WHITE text-sm">Already have an account</Button>
+      <View className="flex items-center justify-start h-[55%] bg-PRIMARY rounded-t-xl px-10 pt-5">
+        <Text className="text-center text-2xl text-WHITE font-[Roboto-Black] pb-2" >Welcome to Course-Tutorial</Text>
+        <Text className="text-center text-sm text-WHITE pb-8">Transform your ideas into engaging learning experiences with AI 🍏 📖</Text>
+        <Button className="w-full capitalize  bg-WHITE py-3  mb-5" textClassName="font-[Roboto-Regular] text-sm">Get Started</Button>
+        <Button className="w-full capitalize bg-PRIMARY border border-WHITE py-3 mb-7" textClassName="font-[Roboto-Regular] text-WHITE text-sm">Already have an account</Button>
       </View>
     </SafeAreaView>
   );
