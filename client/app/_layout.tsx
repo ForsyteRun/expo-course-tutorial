49417105fdbc24bf4e4eba0ca9b31d73527050dc
forsyteru <forsyterun@gmail.com>
2025-09-04 07:41:49 +0200

    feat: Toast

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,14 +1,27 @@
+import Toast from "@/components/Toast";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
-  useFonts({
+  const [loaded] = useFonts({
     "Roboto-Black": require("./../assets/fonts/RobotoCondensed-Black.ttf"),
     "Roboto-Regular": require("./../assets/fonts/RobotoCondensed-Regular.ttf"),
     "Roboto-Italic": require("./../assets/fonts/RobotoCondensed-Italic.ttf"),
   });
 
+  if (!loaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+
   return (
-    <Stack screenOptions={{ headerShown: false, animation: "fade" }}></Stack>
+    <>
+      <Stack screenOptions={{ headerShown: false, animation: "fade" }}></Stack>
+      <Toast />
+    </>
   );
 }
