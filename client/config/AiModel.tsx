01714a131943633d0dc96aaba43478481b0dfc8a
forsyteru <forsyterun@gmail.com>
2025-09-04 12:04:03 +0200

    fix: generateFromGemini

--- client/config/AiModel.tsx
+++ client/config/AiModel.tsx
@@ -1,11 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
-export type AIResponse = {
-  textOutputs: string[];
-  imageBase64: string[];
-};
-
-export async function generateContentFromGemini(input: string) {
+export async function generateFromGemini(input: string) {
   const ai = new GoogleGenAI({ apiKey: process.env.EXPO_PUBLIC_HF_TOKEN });
 
   const contents = [
@@ -16,13 +11,30 @@ export async function generateContentFromGemini(input: string) {
 
   try {
     const response = await ai.models.generateContent({
+      // model: "gemini-2.5-flash-lite",
       model: "models/gemini-2.0-flash",
       contents,
     });
 
-    const candidate = response.candidates?.[0]?.content;
-    console.log("AI response:", candidate);
-  } catch (error) {
-    console.log("error:", error);
+    const candidate = response.candidates?.[0]?.content?.parts?.[0]?.text;
+
+    return { success: true, candidate };
+  } catch (err) {
+    let error = "";
+
+    if (err instanceof Error) {
+      try {
+        const parsed = JSON.parse(err.message);
+        error = parsed?.error?.message || err.message;
+      } catch {
+        console.log("Unknown error parsing JSON:", err.message);
+        error = err.message;
+      }
+    } else {
+      console.log("Unknown error type:", JSON.stringify(err));
+      error = JSON.stringify(err);
+    }
+
+    return { success: false, error };
   }
 }
