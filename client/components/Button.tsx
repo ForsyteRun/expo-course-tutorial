b5792f3391085371d9e77070e1180e4216cc2d79
forsyteru <forsyterun@gmail.com>
2025-09-04 12:02:28 +0200

    fix: button

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -1,14 +1,29 @@
-import cn from 'clsx'
-import { FC, PropsWithChildren } from 'react'
-import { ActivityIndicator, Pressable, Text } from 'react-native'
-import { IButton } from './button.interaface'
+import cn from "clsx";
+import { FC, PropsWithChildren } from "react";
+import { ActivityIndicator, Pressable, Text } from "react-native";
+import { IButton } from "./button.interaface";
 
-const Button: FC<PropsWithChildren<IButton>> = ({ children, className, textClassName, isLoading, indicatorColor, ...rest }) => {
+const Button: FC<PropsWithChildren<IButton>> = ({
+  children,
+  className,
+  textClassName,
+  isLoading,
+  indicatorColor,
+  ...rest
+}) => {
   return (
-    <Pressable className={cn('rounded', className)} {...rest} disabled={isLoading}>
-      {isLoading ? <ActivityIndicator color={indicatorColor} /> : <Text className={cn('text-center', textClassName)}>{children}</Text>}
+    <Pressable
+      className={cn("rounded", className)}
+      {...rest}
+      disabled={isLoading}
+    >
+      {isLoading ? (
+        <ActivityIndicator color={indicatorColor} />
+      ) : (
+        <Text className={cn("text-center", textClassName)}>{children}</Text>
+      )}
     </Pressable>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
