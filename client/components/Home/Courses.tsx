c9c0e95f669018ca4f40e99015693684f3b2fc5e
forsyteru <forsyterun@gmail.com>
2025-09-05 10:54:29 +0200

    feat: Courses

--- client/components/Home/Courses.tsx
+++ client/components/Home/Courses.tsx
@@ -1,18 +1,40 @@
-import { View, Text, FlatList } from "react-native";
-import React, { FC } from "react";
+import { IMAGES } from "@/constants/images";
 import type { ICourse } from "@/types/course.interface";
-
+import Feather from "@expo/vector-icons/Feather";
+import { FC } from "react";
+import { Dimensions, FlatList, Image, Text, View } from "react-native";
 interface ICoursesProps {
   courses: ICourse[];
 }
 
 const Courses: FC<ICoursesProps> = ({ courses }) => {
+  const screenWidth = Dimensions.get("window").width;
+
   return (
-    <View>
+    <View className="px-4">
+      <Text className="text-2xl font-[Roboto-Black] mb-5">Courses</Text>
       <FlatList
         data={courses}
+        horizontal
+        showsHorizontalScrollIndicator={false}
         keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => (
+          <View className="flex items-start justify-start gap-3 p-4 bg-BG_GRAY rounded-2xl mr-4">
+            <Image
+              style={{ width: screenWidth - 60 }}
+              source={IMAGES[item.image as keyof typeof IMAGES]}
+              className="w-full h-[200px] rounded-lg"
+              resizeMode="cover"
+            />
+            <View style={{ width: screenWidth - 70 }}>
+              <Text className="text-lg mt-2">{item.title}</Text>
+              <View className="flex-row items-center justify-start gap-2 mt-4">
+                <Feather name="book-open" size={20} color="black" />
+                <Text className="text-lg">{item.content.length} Chapters</Text>
+              </View>
+            </View>
+          </View>
+        )}
       />
     </View>
   );
---
+++ client/constants/images.ts
@@ -0,0 +1,24 @@
+export const IMAGES = {
+  "1": require("@/assets/images/courses/1.webp"),
+  "2": require("@/assets/images/courses/2.webp"),
+  "3": require("@/assets/images/courses/3.webp"),
+  "4": require("@/assets/images/courses/4.webp"),
+  "5": require("@/assets/images/courses/5.webp"),
+  "6": require("@/assets/images/courses/6.webp"),
+  "7": require("@/assets/images/courses/7.webp"),
+};
+
+export const practiceOptions = [
+  {
+    name: "Quiz",
+    value: require("@/assets/images/courses/8.webp"),
+  },
+  {
+    name: "Flashcards",
+    value: require("@/assets/images/courses/9.webp"),
+  },
+  {
+    name: "Question 7 ans",
+    value: require("@/assets/images/courses/10.webp"),
+  },
+];
