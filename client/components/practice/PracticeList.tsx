b734f662e6d441818b46378f168abdfa9196ca57
forsyteru <forsyterun@gmail.com>
2025-09-08 11:46:25 +0200

    fix: PracticeList loader

--- client/components/practice/PracticeList.tsx
+++ client/components/practice/PracticeList.tsx
@@ -1,4 +1,11 @@
-import { View, Text, ImageSourcePropType, Image, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  ImageSourcePropType,
+  Image,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import React, { FC } from "react";
 import { useDataFromStorage } from "@/hooks/useDataFromStorage";
 
@@ -7,22 +14,28 @@ interface IPracticeListProps {
 }
 
 const PracticeList: FC<IPracticeListProps> = ({ icon }) => {
-  const { courses } = useDataFromStorage();
+  const { courses, loading } = useDataFromStorage();
 
   return (
-    <FlatList
-      data={courses}
-      numColumns={2}
-      keyExtractor={(_, index) => index.toString()}
-      showsVerticalScrollIndicator={true}
-      renderItem={({ item }) => (
-        <View className="w-[45%] flex items-center m-2">
-          <Image source={icon} className="w-20 h-20" />
-          <Text className="text-[10px] text-center">{item.title}</Text>
-        </View>
+    <View className="flex-1">
+      {loading ? (
+        <ActivityIndicator size={"large"} color={"red"} className="flex-1" />
+      ) : (
+        <FlatList
+          data={courses}
+          numColumns={2}
+          keyExtractor={(_, index) => index.toString()}
+          showsVerticalScrollIndicator={true}
+          renderItem={({ item }) => (
+            <View className="w-[45%] flex items-center m-2">
+              <Image source={icon} className="w-20 h-20" />
+              <Text className="text-[10px] text-center">{item.title}</Text>
+            </View>
+          )}
+          contentContainerStyle={{ padding: 10 }}
+        />
       )}
-      contentContainerStyle={{ padding: 10 }}
-    />
+    </View>
   );
 };
 
