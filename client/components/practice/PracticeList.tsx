9ffa7351944ff0034b9a840f3bb45ae7abe4d6cc
forsyteru <forsyterun@gmail.com>
2025-09-08 12:10:42 +0200

    fix: PracticeList

--- client/components/practice/PracticeList.tsx
+++ client/components/practice/PracticeList.tsx
@@ -8,7 +8,7 @@ import {
 } from "react-native";
 import React, { FC } from "react";
 import { useDataFromStorage } from "@/hooks/useDataFromStorage";
-
+import AntDesign from "@expo/vector-icons/AntDesign";
 interface IPracticeListProps {
   icon: ImageSourcePropType;
 }
@@ -17,7 +17,7 @@ const PracticeList: FC<IPracticeListProps> = ({ icon }) => {
   const { courses, loading } = useDataFromStorage();
 
   return (
-    <View className="flex-1">
+    <View className="flex-1 bg-BG_GRAY">
       {loading ? (
         <ActivityIndicator size={"large"} color={"red"} className="flex-1" />
       ) : (
@@ -25,14 +25,23 @@ const PracticeList: FC<IPracticeListProps> = ({ icon }) => {
           data={courses}
           numColumns={2}
           keyExtractor={(_, index) => index.toString()}
-          showsVerticalScrollIndicator={true}
+          showsVerticalScrollIndicator={false}
           renderItem={({ item }) => (
-            <View className="w-[45%] flex items-center m-2">
+            <View className="w-[48%] relative items-center bg-WHITE m-1 p-4 rounded-2xl">
+              <AntDesign
+                name="checkcircle"
+                size={18}
+                color="#999"
+                className="absolute top-2 right-2"
+              />
               <Image source={icon} className="w-20 h-20" />
-              <Text className="text-[10px] text-center">{item.title}</Text>
+              <Text className="text-[14px] text-center">{item.title}</Text>
             </View>
           )}
-          contentContainerStyle={{ padding: 10 }}
+          contentContainerStyle={{
+            padding: 10,
+            justifyContent: "center",
+          }}
         />
       )}
     </View>
